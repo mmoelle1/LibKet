307732152255809d8fcabfa89b5b96cd1b925492
Matthias Moeller <m.moller@tudelft.nl>
2020-07-20 15:41:52 +0200

    Updated logo

--- notebooks/cxx/LibKet.hpp
+++ notebooks/cxx/LibKet.hpp
@@ -1,2 +1,12 @@
+#pragma onc
 
+#pragma cling add_include_path("/home/jovyan/LibKet/external/armadillo")
+#pragma cling add_include_path("/home/jovyan/LibKet/libket")
 
+#pragma cling add_library_path("/home/jovyan/LibKet/build/external/armadillo")
+#pragma cling add_library_path("/home/jovyan/LibKet/build/external/QuEST/QuEST")
+
+#pragma cling load("libarmadillo.so")
+#pragma cling load("libQuEST.so")
+
+#include "/home/jovyan/LibKet/libket/LibLet.hpp"
